bd6d187788a35fdd11f9577280953f12f4b191ab
BlurryGames <julian.lb.games@gmail.com>
2024-09-12 16:18:35 +0200

    Lesson27: Variables - Part 1

--- Scripts/intro_to_scripting.gd
+++ Scripts/intro_to_scripting.gd
@@ -1,1 +1,15 @@
 class_name IntroToScripting extends Node2D
+
+var ability: String = "slash"
+
+var move_speed: float = 2.53
+
+var score: int = 5
+
+var game_over: bool = false
+
+func _ready() -> void:
+	move_speed = 5.1
+	print(move_speed)
+	move_speed = 500.156
+	print(move_speed)
