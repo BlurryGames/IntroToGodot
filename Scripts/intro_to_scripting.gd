d7b9502ab07c27e74db5991114712ceba628432f
BlurryGames <julian.lb.games@gmail.com>
2024-09-12 17:36:57 +0200

    Lesson36: Conditions - Part 1

--- Scripts/intro_to_scripting.gd
+++ Scripts/intro_to_scripting.gd
@@ -1,10 +1,18 @@
 class_name IntroToScripting extends Node2D
 
-var money: int = 10
+var score: int = 5
 
 func _ready() -> void:
-	money += 5
-	money *= 2
-	money -= 3
-	money /= 2
-	print(money)
+	if score == 10:
+		print("You win!")
+	
+	if score >= 5:
+		print("Score is greater than 5")
+	
+	var a: int = 50
+	var b: int = 50
+	if a < b:
+		print("a is less than b")
+	
+	if a != b:
+		print("a is not equal to b")
